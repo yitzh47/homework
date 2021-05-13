4544ee166d6039c70ed0dc31e6f94ad5c59afa8d
Your Nameyitzh47 <yitzhertz47@gmail.com>
2021-05-12 20:07:20 -0400

    try

---
+++ python/44/class.py
@@ -0,0 +1,41 @@
+name = "Joe Biden \"kamala\""
+name2 = 'Kamala Harris\' \\ \nnew line'
+print(name, name2)
+
+autoConcatenate = 'one' 'two''three' 'four'
+print(autoConcatenate)
+
+print('''This is 
+2
+3
+4
+''')
+
+print(name.upper())
+
+print(name2.lower())
+
+print('what the heck' .title())
+
+print(name.replace('Joe', 'newname'))
+
+print(name)
+first = 'f'
+last = 'l'
+
+
+print(f'First: {first} last: {last}')
+print('First: {} last: {}'.format(first, last))
+
+
+students = ['joe', 'donald', 'barak']
+
+print(len(name))
+
+print(name[-2])
+
+print(name[2:16:2])
+
+print(name[::1])
+
+print(name[::-1])
