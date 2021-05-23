02da9c0c0f66be14d683ebf6d4693dd830d694c5
Your Nameyitzh47 <yitzhertz47@gmail.com>
2021-05-23 10:09:46 -0400

    hw46

---
+++ 46/hw46.py
@@ -0,0 +1,38 @@
+class die():
+
+    def __init__(self, sides=6):
+        self.sides = sides
+
+    def roll(self):
+        import random
+        result = random.randint(1, self.sides)
+        return result
+
+
+x = die(12)
+print(f'The die rolled a {x.roll()}.')
+
+
+class SixSidedDie(die):
+    sides = 6
+
+    def roll(self):
+        import random
+        result = random.randint(1, self.sides)
+        return result
+
+
+x = SixSidedDie()
+print(f'The die rolled a {x.roll()}.')
+
+
+the_list = [5, 8, 7, 3, 9, 0, 4, 2, 1]
+print(the_list)
+
+sort = len(the_list) - 1
+for i in range(sort):
+    for x in range(0, sort - i):
+        if the_list[x] > the_list[x+1]:
+            the_list[x], the_list[x+1] = the_list[x+1], the_list[x]
+
+print(the_list)
