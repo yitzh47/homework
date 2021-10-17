78ad13f272fd0d0b162a98a7c076d56626637229
yitzh47 <yitzhertz47@gmail.com>
2021-10-17 19:25:25 -0400

    70 done

--- javascript/70/hw_70.js
+++ javascript/70/hw_70.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    const formBox = $('<div id="form"></div>');      
+    const formBox = $('<div id="form"></div>');
     formBox.css('width', '250px');
     formBox.css('height', '300px');
     formBox.css('margin', 'auto');
@@ -11,23 +11,18 @@
     formBox.append('<input id="checkLicense" type="checkbox">');
     formBox.append('<button id="subButton" disabled>submit</button>');
 
-    formBox.appendTo('body'); 
+    formBox.appendTo('body');
     const name = $("#name");
     const address = $("#address");
     const subButton = $("#subButton");
     const checkLicense = $("#checkLicense");
 
     subButton.on('click', () => {
-        const displayName = name.val();
-        const displayaddress = address.val();
-
-        $(`<div>Name: ${name.val()} <br> Address: ${address.val()} </div>`).appendTo('body');
+        $('body').append(`<div>Name: ${name.val()}<br>Address: ${address.val()} </div>`);
     });
 
     let trueOrFalse = 1;
-
-    checkLicense.change(function() {
+    checkLicense.change(function () {
         subButton.prop('disabled', ++trueOrFalse % 2);
-        }
-    );
+    });
 }());
