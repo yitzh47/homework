8f82d5bfcbd28f5cf6f36f3a768b9043388c8107
yitzh47 <yitzhertz47@gmail.com>
2021-11-09 21:35:26 -0500

    hw78

---
+++ javascript/78/hw_78.js
@@ -0,0 +1,39 @@
+(function() {
+    'use strict';
+
+    function Vehicle(color){
+        this.color = color;
+    }
+
+    Vehicle.prototype.go = function(speed){
+        this.speed = speed;
+        console.log(`now going at speed ${this.speed}mph`);
+    };
+
+    Vehicle.prototype.print = function () {
+        console.log(`The vehicles color is ${this.color}, while the vehicles speed is ${this.speed}`);
+    };
+
+    function Plane(color) {
+        this.color = color;
+    }
+    Plane.prototype = Object.create(Vehicle.prototype);
+
+    Plane.prototype.go = function (speed) {
+        this.speed = speed;
+        console.log(`now FLYING at speed ${this.speed}mph`);
+    };
+
+    const v = new Vehicle("red");
+    v.go("100");
+    v.print();
+
+    const v2 = new Vehicle("red");
+    v2.go("100");
+    v2.print();
+
+
+    const p = new Plane("black");
+    p.go("500");
+    p.print();
+}());
