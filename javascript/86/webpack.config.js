9e6902872a4fbcc64dca2f31782af0358e27592e
yitzh47 <yitzhertz47@gmail.com>
2021-12-22 19:32:10 -0500

    hw86

---
+++ javascript/86/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'HW86',
+        template: './src/index.html'
+    }),
+    //// banner not working
+    new webpack.BannerPlugin("hi")
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+}
